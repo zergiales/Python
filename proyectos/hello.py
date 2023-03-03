15b689ec722dce38733a9fab5fb5edf8110b87fd
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-03-03 20:09:18 +0100

    caracteres

--- proyectos/hello.py
+++ proyectos/hello.py
@@ -5,3 +5,16 @@ Estos es
 para hacer un comentario
 de varias lineas
 """
+# Cadenas de caracteres String
+'Sabroso'
+'ESPAÑA'
+'Python'
+
+# Valores Booleanos
+True
+False
+
+# Hacer listas
+[0,1,2,3,4,5] # todos son del mismo valor , que en este caso es numerico
+['Leche','cacao','avellanas','azucar']
+['diez',10,'nueve',9] # tambien se puede hacer con distintas variables
