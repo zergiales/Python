53e8bb289cecac5d07b03afd49bbd1f02283b322
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-03-25 10:59:04 +0100

    pruebas con spider

--- proyectos/hello.py
+++ proyectos/hello.py
@@ -1,5 +1,5 @@
 # Hola mundo
-print("prueba desde visual holaa")
+print("na tranquii")
 """
 Estos es 
 para hacer un comentario
@@ -18,3 +18,11 @@ de varias lineas
 [0,1,2,3,4,5] # todos son del mismo valor , que en este caso es numerico
 ['Leche','cacao','avellanas','azucar']
 ['diez',10,'nueve',9] # tambien se puede hacer con distintas variables
+
+# ejemplo con spider 
+def hello():
+    """"Imprimir una cadena de caracteres"""
+    print("mannnnn")
+#llamamos a la funcion 
+hello()
+    
